66eb2900c98bd3e5dd3de154e47f2b083779c083
Sadhana-Malik <sadhanacool35@gmail.com>
2021-10-21 08:49:11 +0530

    update

---
+++ Operators_Boolean variables.py
@@ -0,0 +1,1 @@
+# Equal == , Not equal x!=y, Bigger than = x>y , Smaller than = x<y
--- index.py
+++ index.py
@@ -1,2 +1,8 @@
 print("Hello Sadhana. You did it")
 print('Now you know how to work with git')
+print("1", "2", "3", sep="-", end="*")
+print(10, 20, 30, sep="&&&&", end="OR")
+print("sadhana")
+
+
+help('print')
